d35bfa793a92468fa0cc37aa916d3bcf6b4cd98b
Ham <ham97@mac.com>
2022-12-09 10:54:36 +0000

    basic updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,26 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_INFURA_KEY || "",
+      accounts:
+        process.env.DEV_PRIVATE_KEY !== undefined
+          ? [process.env.DEV_PRIVATE_KEY]
+          : [],
+      gas: 2220000000,
+      gasPrice: 80000000000,
+    },
+  },
 };
 
 export default config;
